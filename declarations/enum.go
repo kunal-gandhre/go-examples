b1befd95651a61b1dd40f6143ce979a2df485981
Kunal Gandhre <gandhre.kunal@gmail.com>
2020-03-06 00:34:42 +0530

    helloword and enum

---
+++ declarations/enum.go
@@ -0,0 +1,44 @@
+package main
+
+package main
+
+import "fmt"
+
+type Enum interface {
+	name() string
+	ordinal() int
+	valueOf() *[]string
+}
+
+type LogLevel uint
+
+func (log LogLevel) name() string {
+	return logs[log]
+}
+func (log LogLevel) ordinal() int {
+	return int(log)
+}
+func (log LogLevel) String() string {
+	return logs[log]
+}
+func (log LogLevel) values() *[]string {
+	return &logs
+}
+
+var logs = []string{"UNSPECIFIED", "TRACE", "INFO", "WARNING", "ERROR"}
+
+const (
+	UNSPECIFIED LogLevel = iota
+	TRACE // 1
+	INFO // 2
+	WARNING // 3
+	ERROR // 4
+)
+
+func main() {
+	fmt.Println(UNSPECIFIED)
+	fmt.Println(TRACE.name())
+	fmt.Println(TRACE.String())
+	fmt.Println(TRACE.ordinal())
+	fmt.Println(*ERROR.values())
+}
